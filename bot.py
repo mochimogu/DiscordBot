1aa41500ce75db372bc1e2e0a70c4835a3f13e79
thao0896 <thao0896@umn.edu>
2024-08-07 16:51:15 -0500

    still working on the api call

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import discord
 import settings
+import requests
 from discord.ext import commands
 
 
 def run():
 
     intents = discord.Intents.default()
+    intents.message_content= True
 
     cilent = commands.Bot(command_prefix="!", intents=intents)
 
@@ -17,10 +19,52 @@ def run():
         print(cilent.user.id)
     
     
-    # @cilent.command()
-    # async def botCommand(ctx):    
+    @cilent.command()
+    async def ping(ctx):    
 
-    #     await ctx.send("HELLO THERE MASTER");
+        await ctx.send("HELLO THERE MASTER");
+    
+    @cilent.command()
+    async def user(ctx, input):
+        await ctx.send(input)
+
+    @cilent.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Handled Error Locally")
+            
+    @cilent.group()
+    async def math(ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f"The {ctx.subcommand_passed} is not a math command")
+            
+    @math.command()
+    async def subtraction(ctx, a : int, b : int):
+        await ctx.send(a - b)
+        
+    @math.command()
+    async def addition(ctx, a : int, b : int):
+        await ctx.send(a + b)
+        
+    @cilent.command()
+    async def getquote(ctx, categories):
+        
+        if isinstance(categories, commands.MissingRequiredArgument):
+            await ctx.send("Here are the possible options for categories")
+        else:
+            
+            url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(categories)
+            response = requests.get(url, headers={'X-api-key' : settings.API_KEY})
+            if response.status_code == requests.codes.ok:
+                print(200)
+                print(response.text)
+                
+                results = response.text[0]
+                print(results)
+                # await ctx.send(results['quote'])                
+            else:
+                print('Error')
+                print(404)
 
     cilent.run(settings.DISCORD_TOKEN);
     
--- settings.py
+++ settings.py
@@ -4,3 +4,4 @@ import os
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+API_KEY = os.getenv("API_KEY")
